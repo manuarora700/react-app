ea499b7845dfca68dfba66107e78d9e58408243d
Manu Arora <mannuarora700@gmail.com>
2018-04-20 03:40:51 +0530

    Code refactoring

--- robots/src/Card.js
+++ robots/src/Card.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ name, email, id}) => {
 	return (
-
-		<div>
-			<img src='http://www.robohash.org/test' alt='robots'/>
+		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<img src={`http://www.robohash.org/${id}?200x200`} alt='robots'/>
 			<div>
-				<h2>John Doe</h2>
-				<p>Johndoe@gmail.com</p>
+				<h2>{name}</h2>
+				<p>{email}</p>
 			</div>
 		</div> // we need to import react to use JSX
 
