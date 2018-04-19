540a593c8920179c95e97d1b9a96f86b8fabe4d3
Manu Arora <mannuarora700@gmail.com>
2018-04-19 23:09:42 +0530

    Working with react

--- robots/src/Hello.js
+++ robots/src/Hello.js
@@ -3,11 +3,11 @@ import './Hello.css';
 class Hello extends Component {
 	render() {
 		return (
-		<div>
+		<div className='f1 tc'>
 			<h1>Hello World</h1>
-			<p>Welcome to react!</p>
+			<p>{this.props.greeting}</p>
 		</div>
-		);
+		); //this is not HTML, this is JSX -- HTML like syntax to create VirtualDOM
 	}
 }
 
--- robots/src/index.js
+++ robots/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import Hello from './Hello.js';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-ReactDOM.render(<Hello />, document.getElementById('root'));
+
+
+ReactDOM.render(<Hello greeting={'Hello' + 'React Ninja'}/>, document.getElementById('root'));
 registerServiceWorker();
