5575b428c48b1a8c018dfab2c0d74adab2d9e6fb
Manu Arora <mannuarora700@gmail.com>
2018-04-19 23:19:12 +0530

    Robohash for random robot images, use http://www.robohash.org/anytingajsndjasn

---
+++ robots/src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = () => {
+	return (
+
+		<div>
+			<img src='http://www.robohash.org/test' alt='robots'/>
+			<div>
+				<h2>John Doe</h2>
+				<p>Johndoe@gmail.com</p>
+			</div>
+		</div> // we need to import react to use JSX
+
+	);
+}
+
+export default Card;
