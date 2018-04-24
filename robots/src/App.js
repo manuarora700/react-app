b8787a0cc61bbf4ac5f949dade30322e73c26ff0
Manu Arora <mannuarora700@gmail.com>
2018-04-24 11:43:28 +0530

    Working with states

--- robots/src/App.js
+++ robots/src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
+//State is a descirption of your app
+//Parent feeds state into a child component
+//State >> props
 
-const App = () => {
-	return (
+class App extends Component {
+	//to add a state
+	constructor() {
+		//this is a state
+		super()
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
+	render() {
+		return (
 		<div className='tc'>
 			<h1>RoboFriends</h1>
 			<SearchBox />
 			<CardList robots = {robots}/>
 		</div>
-	)
+	);
+	}
 }
 export default App;
